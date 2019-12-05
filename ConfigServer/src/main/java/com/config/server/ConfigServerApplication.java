1e11c2cb91e63ebe4878bcf4c80e93143786650a
rjosula <rjosula@nisum.com>
2019-12-05 19:12:58 +0530

    Adding Cloud config application.

---
+++ ConfigServer/src/main/java/com/config/server/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * Config server which registers with Git Cental repositoy and gets the properties.
+ * @author rjosula
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
